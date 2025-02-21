8481c25725096c45994073a0703a048d01154302
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-02-21 02:54:14 -0300

    New commit

---
+++ TheRightShift.java
@@ -0,0 +1,18 @@
+public class TheRightShift {
+    /*
+
+        When a value has bits that are "shifted off", those bits are lost. For example, the next code
+        fragment shifts the value of 3 to the right two positions, which causes the two low-order bits
+        to be lost, resulting again in a being set to 8:
+
+        int a = 35;
+        a = a >> 2; //a now contains 8
+
+        looking at the same operation in binary shows more clearly how this happens:
+
+        00100011    35
+        >> 2
+        00001000    8
+
+     */
+}
