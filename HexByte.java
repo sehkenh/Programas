bbb7403fc3a917e6b21c7302a4f0de7f779806ff
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-02-22 05:21:41 -0300

    New commit

--- HexByte.java
+++ HexByte.java
@@ -1,4 +1,14 @@
 //Masking sign extension.
 public class HexByte {
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        char [] hex = {
+                '0', '1', '2', '3', '4', '5', '6', '7',
+                '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+        };
+
+        byte b = (byte) 0xf1;
+
+        System.out.println("b = 0x" +hex[(b >> 4) & 0x0f] + hex[b & 0x0f]);
+    }
 }
+//Search for more binary & hex problems
