e342a8b85318e9c4ee6f0678664f4dc1cae51c6e
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-02-23 15:08:21 -0300

    New commit

---
+++ UnsignedRightShift.java
@@ -0,0 +1,12 @@
+public class UnsignedRightShift {
+    /*
+
+        int a = -1
+        a = a >>> 24;
+
+        11111111 11111111 11111111 11111111 -1 in binary as an int
+        >>> 24
+        00000000 00000000 00000000 11111111 255 in binary as an int
+
+     */
+}
