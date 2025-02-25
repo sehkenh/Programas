3b77bb31702f6f29e007febbf1de8ac33a7029cf
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-02-25 03:59:04 -0300

    New commit

---
+++ RelationalOperators.java
@@ -0,0 +1,25 @@
+public class RelationalOperators {
+    /*
+
+        int a = 4;
+        int b = 1;
+
+        boolean c = a < b;
+
+        in this case, the result of a < b(which is false) is stored in c.
+
+        If you are coming from C/C++ background, please note the following. In C/C++, these
+        types of statements are very common:
+
+        int done;
+        //..
+        if(!done)... // Valid in C/C++
+        if(done)...  // but not in Java.
+
+        In Java these statements must be written like this:
+
+        if(done == 0)...  //This is Java-style
+        if(done != 0)...
+
+     */
+}
