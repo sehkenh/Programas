dab55e8c79ea10616a6d8f9fa0f9df01092dad0d
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-02-25 05:37:57 -0300

    New commit

--- ShortCircuitLogicalOperators.java
+++ ShortCircuitLogicalOperators.java
@@ -1,11 +1,11 @@
 public class ShortCircuitLogicalOperators {
     /*
 
-        if (denom !=0 && nume / denom > 10 )
+        if (denom !=0 && num / denom > 10 )
 
         Since the short-circuit form of AND(&&)is used, there is no risk of causing a run-time
         exception when denom is zero. If this line of code were written using the single & version of
-        AND, both sides would be evaluated, causing arunt-time exception when denom is zero.
+        AND, both sides would be evaluated, causing a run-time exception when denom is zero.
 
         However, there are exceptions to this rule. For example, consider the following statement:
 
