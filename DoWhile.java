b7ede4f9b50e7c302854e8bfb370a12770bdfc35
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-02-28 23:18:00 -0300

    New commit

--- DoWhile.java
+++ DoWhile.java
@@ -9,3 +9,12 @@ public class DoWhile {
         }while (n > 0);
     }
 }
+/*
+
+    The loop in the preceding program, while technically correct, can be written more efficiently as follows:
+
+    do{
+        System.out.println("tick " +n);
+    } while(--n > 0);
+
+ */
