a9b04a9bacafe345adfb697d995ceae6f34bddf2
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-03-02 10:17:57 -0300

    New commit

--- ForTick.java
+++ ForTick.java
@@ -7,3 +7,20 @@ public class ForTick {
             System.out.println("tick "+ n);
     }
 }
+/*
+
+    It is possible to declare the variable inside the initialization portion of the for. For example, here is the preceding
+    program recoded so that the loop control variable n is declared as an int inside the for.
+
+    // Declare a loop control variable inside the for.
+
+    public class ForTick{
+        public static void main (String[] args){
+
+            //here n is declared inside of the for loop
+            for (int n = 10; n > 0; n--)
+                System.out.print("tick " +n);
+        }
+    }
+
+ */
