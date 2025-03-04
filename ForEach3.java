740e33b3ee0dfd440440fc53fa7da0070916dd3b
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-03-04 05:52:49 -0300

    New commit

--- ForEach3.java
+++ ForEach3.java
@@ -8,6 +8,7 @@ public class ForEach3 {
         for(int i = 0; i < 3; i++)
             for(int j = 0; j < 5; j++)
                 nums[i][j] = (i+1)*(j+1);
+
         //use for-each for to display and sum the values
         for (int[] x : nums){
             for (int y : x){
