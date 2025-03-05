5e9e1042d1bec949fab9d9f815e3541d7f4f61bd
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-03-05 07:01:51 -0300

    New commit

--- BreakLoop4.java
+++ BreakLoop4.java
@@ -4,7 +4,7 @@ public class BreakLoop4 {
         outer: for(int i= 0; i<3; i++) {
             System.out.print("Pass " + i + ": ");
             for (int j = 0; j < 100; j++) {
-                if (j == 10) break outer; //exit both loops
+                if (j == 50) break outer; //exit both loops
                 System.out.print(j + " ");
             }
             System.out.println("This will not print");
