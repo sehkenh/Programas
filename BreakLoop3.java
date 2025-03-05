35b2378eda949d8ca30b52efb8168c5b9ac3adce
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-03-05 01:46:02 -0300

    New commit

--- BreakLoop3.java
+++ BreakLoop3.java
@@ -3,8 +3,8 @@ public class BreakLoop3 {
     public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
             System.out.print("Pass " + i +": " );
-            for (int j = 0; j < 100; j++) {
-                if (j == 10) break;
+            for (int j = 0; j < 101; j++) {
+                if (j == 101) break;
                 System.out.print(j + " ");
             }
             System.out.println();
