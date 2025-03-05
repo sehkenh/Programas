47d6e3c7ef983152321ab5eb2fa9b48dedfc0440
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-03-05 14:14:14 -0300

    New commit

---
+++ Box.java
@@ -0,0 +1,9 @@
+//class Box {
+//    double width;
+//    double height;
+//    double depth;
+//}
+//
+//Box myBox = new Box(); //Create a boc object called myBox
+//
+//myBox.width = 100;
---
+++ BoxDemo.java
@@ -0,0 +1,5 @@
+public class BoxDemo {
+    public static void main(String[] args) {
+
+    }
+}
