f0dd13309a019b510a7e78abd9bfa2718f7d85b5
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-03-05 15:24:05 -0300

    New commit

--- BoxDemo.java
+++ BoxDemo.java
@@ -1,5 +1,29 @@
+/*
+
+    A program that uses the Box class.
+
+    Call this file BoxDemo.java
+
+ */
+
+class Box{
+    double width;
+    double height;
+    double depth;
+}
+
 public class BoxDemo {
     public static void main(String[] args) {
+        Box myBox = new Box();
 
+        //assign values to mybox's instance variables
+        myBox.width = 10;
+        myBox.height = 20;
+        myBox.depth = 15;
+
+        //compute volume of box
+        double vol = myBox.width * myBox.height * myBox.depth;
+
+        System.out.println("Volume is: " + vol);
     }
 }
