ac7597b76f648898932864f35c7345d151d4e193
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-03-07 12:48:27 -0300

    New Commit

--- MethodwithParameters.java
+++ MethodwithParameters.java
@@ -5,11 +5,22 @@ public class MethodwithParameters {
             return 10 * 10;
         }
 
-        Better Version
+        /////////////// Better Version ///////////////
 
         int square(int i){
             return i * i
         }
 
+        Now, square() will return the square of whatever value it is called with. That is, square()
+        is now a general-purpose method that can compute the square of any integer value, rather than just 10.
+
+        Here is an example:
+
+        int x, y
+
+        x = square(5);   25
+        x = square(9);   81
+        y = 2
+        x = square(y);    4
      */
 }
