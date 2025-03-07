989a1911407c916e6cf3d23fe14d61fc8e3d20c2
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-03-07 12:32:34 -0300

    New Commit

---
+++ MethodwithParameters.java
@@ -0,0 +1,15 @@
+public class MethodwithParameters {
+    /*
+
+        int square(){
+            return 10 * 10;
+        }
+
+        Better Version
+
+        int square(int i){
+            return i * i
+        }
+
+     */
+}
