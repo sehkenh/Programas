c52fd168152f09423e23121baded50324a9d2156
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-03-07 20:39:18 -0300

    Parameterized Constructor

---
+++ BoxDemo7.java
@@ -0,0 +1,38 @@
+/*
+
+    Here, Box uses a parameterized constructor to initialize
+    the dimensions of a box.
+
+ */
+class Box7 {
+    double width;
+    double height;
+    double depth;
+
+    //This is the constructor for Box.
+    Box7(double w, double h, double d){
+        width = w;
+        height = h;
+        depth = d;
+    }
+
+    //Compute and return volume
+    double volume(){
+        return width*height*depth;
+    }
+
+}
+
+public class BoxDemo7 {
+    public static void main(String[] args) {
+        Box7 mybox1 = new Box7(10,20,15);
+        Box7 mybox2 = new Box7(3,6,9);
+
+        double vol;
+
+        vol = mybox1.volume();
+        System.out.println(vol);
+        vol = mybox2.volume();
+        System.out.println(vol);
+    }
+}
