9046ef3e1387ac1f2973ccb18696da64bd6ef31e
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-03-12 16:44:32 -0300

    Private stck and tos

---
+++ Stack2.java
@@ -0,0 +1,25 @@
+class Stack2 {
+    private int [] stck = new int [10];
+    private int tos;
+
+    Stack2() {
+        tos = -1;
+    }
+
+    void push(int item) {
+        if (tos == 9) {
+            System.out.println("Stack is full");
+        } else {
+            stck[++tos] = item;
+        }
+    }
+
+    int pop() {
+        if (tos < 0) {
+            System.out.println("Stack is empty");
+            return 0;
+        }
+        else
+            return stck[tos--];
+    }
+}
