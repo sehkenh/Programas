1b5f350f1a40665a56fdaa411f0526f961a7b90f
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-03-12 16:48:49 -0300

    Private stck and tos, comments at bottom makes the use of stack not legal.

--- Stack2.java
+++ Stack2.java
@@ -23,3 +23,19 @@ class Stack2 {
             return stck[tos--];
     }
 }
+
+class TestStack2 {
+    public static void main(String[] args) {
+        Stack2 mystack1 = new Stack2();
+        Stack2 mystack2 = new Stack2();
+
+        for (int i = 0; i < 10; i++) mystack1.push(i);
+        for (int i = 0; i < 20; i++) mystack2.push(i);
+
+        System.out.println("Popping elements from mystack1" + mystack1.pop());
+        System.out.println("Popping elements from mystack2" + mystack2.pop());
+
+//        mystack1.tos =-2;
+//        mystack2.stck[3] = 100;
+    }
+}
