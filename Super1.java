f0210450a561ab50dffe6a10d8f8532be54ef898
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-03-21 05:38:21 -0300

    Super

---
+++ Super1.java
@@ -0,0 +1,12 @@
+//public class Super1 {
+//    //Boxweight now uses super to initialize its Box attributes.
+//    class Boxweight extends Box10{
+//        double weight; //weight of box
+//    }
+//
+//    //initialize width, height, and depth using super()
+//    BoxWeight(double w, double h, double d, double m){
+//        super(w,h,d);//call superclass constructor
+//        weight = m;
+//    }
+//}
