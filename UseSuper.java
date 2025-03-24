6097cb7398c80a965ac75ea4afa1c3eb7ce29042
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-03-24 11:37:31 -0300

    Use super to overcome hiding.

---
+++ UseSuper.java
@@ -0,0 +1,27 @@
+//Using super to overcome name hiding.
+class A3{
+    int i;
+}
+
+//Create a subclass by extending class A.
+class B3 extends A3{
+    int i;//This i hides the i in A
+
+    B3(int a, int b){
+        super.i = a; //i in A
+        i = b; // i in B
+    }
+
+    void show(){
+        System.out.println(super.i);
+        System.out.println(i);
+    }
+}
+
+public class UseSuper {
+    public static void main(String[] args) {
+        B3 subOb = new B3(1, 2);
+
+        subOb.show();
+    }
+}
