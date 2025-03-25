44f5633263166761195474e46f2ef975363f5a5d
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-03-25 10:07:31 -0300

    Method overriding

---
+++ Override.java
@@ -0,0 +1,38 @@
+//Method overriding
+
+class A5 {
+    int i, j;
+
+    A5(int a, int b){
+        i = a;
+        j = b;
+    }
+
+    //display i and j.
+
+    void show(){
+        System.out.println("i = " + i + ", j = " + j);
+    }
+}
+
+class B5 extends A5{
+    int k;
+
+    B5(int a, int b, int c){
+        super(a, b);
+        k = c;
+    }
+
+    //display k - this overrides show() in A5
+
+    void show(){
+        System.out.println("k = " + k);
+    }
+}
+
+public class Override {
+    public static void main(String[] args) {
+        B5 subOb = new B5(1,2,2);
+        subOb.show(); // this call show() in B5.
+    }
+}
