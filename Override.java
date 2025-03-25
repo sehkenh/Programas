c859ae4e63bcf7016353e7cc5e207fe6f5ed79fe
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-03-25 10:24:11 -0300

    Method overriding

--- Override.java
+++ Override.java
@@ -32,7 +32,7 @@ class B5 extends A5{
 
 public class Override {
     public static void main(String[] args) {
-        B5 subOb = new B5(1,2,2);
+        B5 subOb = new B5(1,2,3);
         subOb.show(); // this call show() in B5.
     }
 }
