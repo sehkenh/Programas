6fcc68ed9b7d67bf3431e2692e8b79e384ceadde
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-04-01 06:29:21 -0300

    Interfaces Implementation through interface reference

---
+++ interface1/IFTest3.java
@@ -0,0 +1,34 @@
+package interface1;
+
+/*
+
+    Create an interface variable and
+    access stacks through it.
+
+ */
+
+class IFTest3 {
+    public static void main(String[] args) {
+        IntStack mystack;
+        DynStack ds = new DynStack(5);
+        FixedStack fs = new FixedStack(8);
+
+        mystack = ds;
+        // push some numbers onto the stack
+        for (int i = 0; i < 12; i++) mystack.push(i);
+
+        mystack = fs; //load fixed stack
+        for (int i = 0; i < 12; i++) mystack.push(i);
+
+        mystack = ds;
+        System.out.println("Values in dynamic stack: ");
+        for (int i = 0; i < 12; i++)
+            System.out.println(mystack.pop());
+
+        mystack = fs;
+        System.out.println("Values in fixed stack: ");
+        for (int i = 0; i < 8; i++)
+            System.out.println(mystack.pop());
+
+    }
+}
