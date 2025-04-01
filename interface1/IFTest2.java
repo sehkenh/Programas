fe59ebd0711856d2344aa0f1c59cf03bc395637d
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-04-01 06:18:21 -0300

    Interfaces Implementation

---
+++ interface1/IFTest2.java
@@ -0,0 +1,70 @@
+package interface1;
+
+/*
+
+    Following is another implementation of IntStack that creates a dynamic stack
+    by use of the same interface definition. In this implementation, each stack
+    is constructed with an initial length. If this initial length is exceeded, then
+    the stack is increased in size. Each time more room is needed, the size of
+    the stack is doubled.
+
+ */
+
+//Implement a "growable" stack.
+
+class DynStack implements IntStack {
+    private int[] stck;
+    private int tos;
+
+    // Allocate and initialize stack
+    DynStack(int size) {
+        stck = new int[size];
+        tos = -1;
+    }
+
+    // Push an item onto the stack
+
+    public void push(int item) {
+        // if stack is full, allocate a larger stack
+        if(tos == stck.length-1) {
+            int [] temp = new int[stck.length * 2]; //double size
+            for(int i = 0; i<stck.length; i++) temp[i] = stck[i];
+            stck = temp;
+            stck[++tos] = item;
+        }
+        else
+            stck[++tos] = item;
+
+    }
+
+    // Pop an item from the stack
+
+    public int pop() {
+        if (tos < 0) {
+            System.out.println("Stack underflow.");
+            return 0;
+        }
+        else
+            return stck[tos--];
+    }
+}
+
+public class IFTest2 {
+    public static void main(String[] args) {
+        DynStack mystack1 = new DynStack(5);
+        DynStack mystack2 = new DynStack(8);
+
+        // these loops cause each stack to grow
+
+        for(int i = 0; i<12; i++) mystack1.push(i);
+        for(int i = 0; i<20; i++) mystack2.push(i);
+
+        System.out.println("Stack in mystack1: ");
+        for(int i = 0; i<12; i++)
+            System.out.print(mystack1.pop() + " ");
+
+        System.out.println("\nStack in mystack2: ");
+        for (int i = 0; i<20; i++)
+            System.out.print(mystack2.pop() + " ");
+    }
+}
