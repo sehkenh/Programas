62fa708e9fb13ae08715602f9495101894561f65
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-04-05 14:35:55 -0300

    Exception Handling - Division by zero(32000 numbers)

--- HandleError.java
+++ HandleError.java
@@ -5,7 +5,7 @@ class HandleError {
         int a = 0, b = 0, c = 0;
         Random r = new Random();
 
-        for(int i = 0; i<32000; i++){
+        for(int i = 0; i<320000; i++){
             try {
                 b = r.nextInt();
                 c = r.nextInt();
