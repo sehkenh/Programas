9400e69f800a4e7758b6c1f167d46cb7b2adaa34
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-04-05 23:29:06 -0400

    Sub and Super catch.

---
+++ SuperSubCatch.java
@@ -0,0 +1,23 @@
+/*
+
+    This program contains an error.
+
+    A subclass must come before its superclass in
+    a series of catch statements. If not,
+    unreachable code will be created and a
+    compile-time error will result.
+
+ */
+
+class SuperSubCatch {
+    public static void main(String[] args) {
+        try {
+            int a = 0;
+            int b = 42 / a;
+        } catch (Exception e) {
+            System.out.println("Generic Exception catch.");
+        }catch (ArithmeticException e) { //This catch is never reached
+            System.out.println("This is never reached.");
+        }
+    }
+}
