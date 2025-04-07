c21c122a08feaac1a2a3f98141765bb7a4e95b89
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-04-07 05:35:34 -0400

    Throw Demo

---
+++ ThrowDemo.java
@@ -0,0 +1,20 @@
+//Demonstrate throw
+
+class ThrowDemo {
+    static void demoproc() {
+        try {
+            throw new NullPointerException("demo"); //java standard exception object
+        } catch (NullPointerException e) {
+            System.out.println("Caught inside demoproc");
+            throw e;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            demoproc();
+        } catch (NullPointerException e) {
+            System.out.println("Recaught: " + e);
+        }
+    }
+}
