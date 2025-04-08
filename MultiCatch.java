81407caa6ffd2c189873f86c053c860345e5e228
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-04-08 08:41:15 -0400

    MultiCatch

---
+++ MultiCatch.java
@@ -0,0 +1,15 @@
+class MultiCatch {
+    public static void main(String[] args) {
+        int a = 10, b = 0;
+        int [] vals = {1,2,3};
+
+        try {
+            int result = a / b;
+            vals[10] = 19;
+        } catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("Caught " + e);
+        }
+
+        System.out.println("After multi-catch");
+    }
+}
